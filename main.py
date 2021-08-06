58d9597fe0e49df3038a9ad423fa98ed386f006f
Fazlul Hoque <sawravm299@gmail.com>
2021-08-06 21:25:16 +0600

    update
    link include

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import scrapetube
 
-videos = scrapetube.get_channel("UC2-BOyC_u8wWPKZ0WIDJ5yw")
+videos = scrapetube.get_channel("UCWg8IGoeU3O0PWDwJFH6DLg")
 url = "https://www.youtube.com/watch?v="
 for video in videos:
+    name = video['title']
     a = video['videoId']
     b = url + str(a)
-    print(b)
+    print(b, name)
